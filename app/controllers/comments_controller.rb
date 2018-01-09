a8ec8f8cba3e134ad85a2a4611f51d37f18d50e4
manishgeniusu <manish@geniusu.com>
2018-01-09 17:46:07 +0530

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,26 @@
 class CommentsController < ApplicationController
-	
+	before_action :set_post
+
 	def create
-		@post = Post.find params[:post_id]
-		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+		@comment = @post.comments.create(comment_params)
 
 		redirect_to post_path(@post)
 	end
 
 	def destroy
-		@post = Post.find params[:post_id]
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
 
 		redirect_to post_path(@post)
 	end
+	
+	private
+		def set_post
+	    @post = Post.find(params[:post_id])
+	  end
+
+	  def comment_params 
+      params.require(:comment).permit(:name, :body)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
+	validates :title, uniqueness: {message: "already taken"}
 	validates :body, presence: true
 
 end
