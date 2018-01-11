535927357942853f158546e92a94dfacb9943744
manishgeniusu <manish@geniusu.com>
2018-01-11 11:41:41 +0530

    styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
 	before_action :set_post
 
+	def new
+		@comment = @post.comments.build
+	end
+
 	def create
 		@comment = @post.comments.create(comment_params)
-
+		if @comment.save
+			flash[:error] = "Saved Successfully!"
+		else
+			flash[:error] = "Can't save, please fill all the details."
+		end
 		redirect_to post_path(@post)
 	end
 
